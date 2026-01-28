74e2b6dbc1f1a531c6206a1163bd1ddb8281d3ae
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-01-28 17:10:24 +0900

    feat: 애플리케이션 실행 시 숫자 야구 게임 실행 로직 구현

--- src/main/java/model/BaseballGame.java
+++ src/main/java/model/BaseballGame.java
@@ -3,4 +3,18 @@ package model;
 public class BaseballGame {
     private final InputView inputView = new InputView();
     private final OutPutView outPutView = new OutputView();
+
+    public void run() {
+        boolean keepPlaying = true;
+        while (keepPlaying) {
+            // 게임 시작
+            playingGame();
+
+            /* 재시작 여부 결정
+                yes 선택 시: askRestart는 true 반환
+                no 선택 시: askRestart는 false 반환
+             */
+            keepPlaying = askRestart();
+        }
+    }
 }
