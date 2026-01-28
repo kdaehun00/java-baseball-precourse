4ddd39c9359b56e3da06a127c97b54196cab7b52
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-01-28 18:17:14 +0900

    feat: 컴퓨터의 정답 3자리 숫자 생성 로직 구현

--- src/main/java/model/BaseballGame.java
+++ src/main/java/model/BaseballGame.java
@@ -1,14 +1,17 @@
 package model;
 
+import java.util.*;
+
 public class BaseballGame {
     private final InputView inputView = new InputView();
     private final OutPutView outPutView = new OutputView();
+    private final Random random = new Random();
 
     public void run() {
         boolean keepPlaying = true;
         while (keepPlaying) {
             // 게임 시작
-            playingGame();
+            playSingleGame();
 
             /* 재시작 여부 결정
                 yes 선택 시: askRestart는 true 반환
@@ -17,4 +20,18 @@ public class BaseballGame {
             keepPlaying = askRestart();
         }
     }
+
+    private void playSingleGame() {
+        List<Integer> targetNum = generateComputerNumber();
+    }
+
+    private List<Integer> generateComputerNumber() {
+        Set<Integer> setNum = new LinkedHashSet<>();
+        while (setNum.size() < 3) {
+            int n = random.nextInt(9) + 1;
+            setNum.add(n);
+        }
+        return new ArrayList<>(setNum);
+    }
+
 }
