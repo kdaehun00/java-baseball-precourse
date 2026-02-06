ed0108a17aa1e16eaf40bef8147611e945ac820f
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-02-06 23:13:54 +0900

    refactor: UserGuess 클래스 내 하드코딩 숫자 제거

--- src/main/java/model/UserGuess.java
+++ src/main/java/model/UserGuess.java
@@ -13,25 +13,29 @@ import java.util.Set;
 public class UserGuess {
     private final List<Integer> digits;
 
+    private static final int DIGIT_COUNT = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
     public static UserGuess from(String input) {
         if (input == null) {
             throw new InvalidInputException(CommonErrorCode.EMPTY_INPUT);
         }
-        if (input.length() != 3) {
+        if (input.length() != DIGIT_COUNT) {
             throw new InvalidInputException(CommonErrorCode.INVALID_LENGTH);
         }
 
-        List<Integer> digits = new ArrayList<>(3);
+        List<Integer> digits = new ArrayList<>(DIGIT_COUNT);
         Set<Integer> dupCheck = new HashSet<>();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < DIGIT_COUNT; i++) {
             char c = input.charAt(i);
             if (!Character.isDigit(c)) {
                 throw new InvalidInputException(CommonErrorCode.NOT_A_NUMBER);
             }
 
             int d = c - '0';
-            if (d < 1 || d > 9) {
+            if (d < MIN_NUMBER || d > MAX_NUMBER) {
                 throw new InvalidInputException(CommonErrorCode.NUMBER_OUT_OF_RANGE);
             }
             if (!dupCheck.add(d)) {
